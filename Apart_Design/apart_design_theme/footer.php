471b24ec52abcac6ae50c06d7a086bcc4a7f1f2e
Robert Li <Robertli5788@gmail.com>
2023-02-24 10:22:47 -0800

    changed header footer

---
+++ Apart_Design/apart_design_theme/footer.php
@@ -0,0 +1,22 @@
+    <footer class="apart-footer">
+        <div class="footer-con">
+            <ul>
+                <li>
+                    <a href="#" title="See Terms and Conditions">Terms</a>
+                </li>
+                <li>
+                    <a href="#" title="See Privacy Policy">Privacy</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="#" title="To About page">About</a>
+                </li>
+                <li>
+                    <a href="#" title="To Contact page">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </footer>
+</body>
+</html>
