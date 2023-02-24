520814e0b8ff58dde9c2ab19529ce282a2ff90aa
Robert Li <Robertli5788@gmail.com>
2023-02-24 13:40:55 -0800

    added archive

--- Apart_Design/apart_design_theme/page-shop.php
+++ Apart_Design/apart_design_theme/page-shop.php
@@ -3,10 +3,9 @@
 ?>
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <div class="col-12">
+
     
     <p><?php the_content(); ?></p>
-
     <?php endwhile; else: ?>
     <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
 <?php endif; ?>
