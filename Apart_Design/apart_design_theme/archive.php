6c4b2112e9716a93679a3e3237bbfe5a2a4d624c
MacolmTann <malcolmtan20@gmail.com>
2023-02-24 14:35:56 -0800

    Update archive.php

--- Apart_Design/apart_design_theme/archive.php
+++ Apart_Design/apart_design_theme/archive.php
@@ -2,13 +2,14 @@
     get_header();
 ?>
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<section id="main">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-    
-    <p><?php the_content(); ?></p>
-    <?php endwhile; else: ?>
-    <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-<?php endif; ?>
+        <p><?php the_content(); ?></p>
+        <?php endwhile; else: ?>
+        <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+    <?php endif; ?>
+</section>
 
 <?php
     get_footer();
