44a4b91ae66434bdbafaa6ef684ffbf5270d2326
MacolmTann <malcolmtan20@gmail.com>
2023-02-24 15:23:31 -0800

    deleted-archive

--- Apart_Design/apart_design_theme/woocommerce.php
+++ Apart_Design/apart_design_theme/woocommerce.php
@@ -1,5 +1,5 @@
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?> 
-    <?php woocommerce_content(); ?>
+    <p><?php the_content(); ?></p>
     <?php endwhile; else: ?>
     <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
 <?php endif; ?>
