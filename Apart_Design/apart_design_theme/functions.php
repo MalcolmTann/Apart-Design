ccd8f1bef6ea7ea8b0beda5859cdcef1c95c4224
MacolmTann <malcolmtan20@gmail.com>
2023-02-24 14:31:07 -0800

    Update functions.php

--- Apart_Design/apart_design_theme/functions.php
+++ Apart_Design/apart_design_theme/functions.php
@@ -25,11 +25,11 @@ remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wra
 add_action('woocommerce_before_main_content', 'aparttheme_wrapper_start', 10);
 add_action('woocommerce_after_main_content', 'aparttheme_wrapper_end', 10);
 
-function my_theme_wrapper_start() {
+function aparttheme_wrapper_start() {
     echo '<section id="main">';
 }
 
-function my_theme_wrapper_end() {
+function aparttheme_wrapper_end() {
     echo '</section>';
 }
 
